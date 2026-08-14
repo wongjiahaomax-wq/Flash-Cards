755d0201eaac65ca783d94c614a7e78363b08e09
wongjiahaomax-wq <wongjiahaomax@gmail.com>
2026-08-14 22:22:22 +0800

    Add sign-out control

---
+++ src/lib/components/SignOutButton.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { goto } from '$app/navigation';
+  import { authClient } from '$lib/auth-client.js';
+
+  let signingOut = $state(false);
+
+  async function signOut() {
+    signingOut = true;
+    await authClient.signOut();
+    await goto('/sign-in');
+  }
+</script>
+
+<button class="button" type="button" onclick={signOut} disabled={signingOut}>
+  {signingOut ? 'Signing out…' : 'Sign out'}
+</button>
