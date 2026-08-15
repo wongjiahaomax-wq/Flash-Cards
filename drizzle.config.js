111b760a6c73ed76cf6d9aba4c6cbae6e456892b
wongjiahaomax-wq <wongjiahaomax@gmail.com>
2026-08-16 07:10:47 +0800

    include import jobs schema in drizzle checks

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/lib/server/db/schema.js',
+  schema: ['./src/lib/server/db/schema.js', './src/lib/server/db/import-job-schema.js'],
   out: './drizzle',
   dialect: 'sqlite',
   verbose: true,
