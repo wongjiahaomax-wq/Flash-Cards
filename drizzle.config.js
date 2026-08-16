8f4b5e6da1991581f7d1b210c0e89b93df502955
wongjiahaomax-wq <wongjiahaomax@gmail.com>
2026-08-16 15:36:41 +0800

    Include tag schema in Drizzle config

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: ['./src/lib/server/db/schema.js', './src/lib/server/db/import-job-schema.js'],
+  schema: [
+    './src/lib/server/db/schema.js',
+    './src/lib/server/db/import-job-schema.js',
+    './src/lib/server/db/tag-schema.js'
+  ],
   out: './drizzle',
   dialect: 'sqlite',
   verbose: true,
