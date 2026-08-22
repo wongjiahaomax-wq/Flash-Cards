13edb12670f3665e16c10702feac3bc7a223e112
wongjiahaomax-wq <wongjiahaomax@gmail.com>
2026-08-22 12:08:40 +0800

    test: load stimulus schema for content guard fixture

--- test/content-guards.test.js
+++ test/content-guards.test.js
@@ -16,6 +16,7 @@ import { createDb } from '../src/lib/server/db/index.js';
 
 const migrationSql = [
   readFileSync(new URL('../drizzle/0000_dashing_centennial.sql', import.meta.url), 'utf8'),
+  readFileSync(new URL('../drizzle/0002_optional_stimulus_groups.sql', import.meta.url), 'utf8'),
   readFileSync(new URL('../drizzle/0006_preview_admin_workspace.sql', import.meta.url), 'utf8')
 ].join('\n').replaceAll('--> statement-breakpoint', '');
 
