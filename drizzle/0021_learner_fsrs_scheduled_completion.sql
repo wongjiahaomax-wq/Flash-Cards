6b837b8162b4b2d94d3be66113c9edb715ea57a3
wongjiahaomax-wq <316963030+wongjiahaomax-wq@users.noreply.github.com>
2026-09-03 11:48:42 +0800

    Fix D1 trigger parsing in scheduled completion migration

--- drizzle/0021_learner_fsrs_scheduled_completion.sql
+++ drizzle/0021_learner_fsrs_scheduled_completion.sql
@@ -11,14 +11,14 @@ ALTER TABLE `scheduled_review_events` ADD COLUMN `resulting_state` integer CHECK
 CREATE TRIGGER `scheduled_review_events_active_guard`
 BEFORE INSERT ON `scheduled_review_events`
 BEGIN
-	SELECT CASE WHEN NEW.`queue_class` IS NULL
+	SELECT (CASE WHEN NEW.`queue_class` IS NULL
 		OR NEW.`run_id` IS NULL
 		OR NEW.`scope_fingerprint` IS NULL
 		OR NEW.`run_started_at` IS NULL
 		OR NEW.`resulting_state` IS NULL
-	THEN RAISE(ABORT, 'scheduled_completion_missing_context') END;
+	THEN RAISE(ABORT, 'scheduled_completion_missing_context') END);
 
-	SELECT CASE WHEN NOT EXISTS (
+	SELECT (CASE WHEN NOT EXISTS (
 		SELECT 1
 		FROM `active_reviews` a
 		WHERE a.`id` = NEW.`id`
@@ -36,23 +36,23 @@ BEGIN
 			AND a.`parameter_revision` = NEW.`parameter_revision`
 			AND a.`scheduler_revision` = NEW.`scheduler_revision`
 			AND a.`scheduler_library_version` = NEW.`scheduler_library_version`
-	) THEN RAISE(ABORT, 'scheduled_completion_active_review_changed') END;
+	) THEN RAISE(ABORT, 'scheduled_completion_active_review_changed') END);
 
-	SELECT CASE WHEN NOT EXISTS (
+	SELECT (CASE WHEN NOT EXISTS (
 		SELECT 1 FROM `active_reviews` a
 		WHERE a.`id` = NEW.`id`
 			AND a.`user_id` = NEW.`user_id`
 			AND a.`revealed_at` IS NOT NULL
-	) THEN RAISE(ABORT, 'scheduled_completion_unrevealed') END;
+	) THEN RAISE(ABORT, 'scheduled_completion_unrevealed') END);
 
-	SELECT CASE WHEN NOT EXISTS (
+	SELECT (CASE WHEN NOT EXISTS (
 		SELECT 1 FROM `active_reviews` a
 		WHERE a.`id` = NEW.`id`
 			AND a.`user_id` = NEW.`user_id`
 			AND a.`expires_at` > cast((julianday('now') - 2440587.5) * 86400000 as integer)
-	) THEN RAISE(ABORT, 'scheduled_completion_expired') END;
+	) THEN RAISE(ABORT, 'scheduled_completion_expired') END);
 
-	SELECT CASE WHEN NOT EXISTS (
+	SELECT (CASE WHEN NOT EXISTS (
 		SELECT 1
 		FROM `learner_fsrs_profiles` p
 		WHERE p.`user_id` = NEW.`user_id`
@@ -61,17 +61,17 @@ BEGIN
 			AND p.`parameter_revision` = NEW.`parameter_revision`
 			AND p.`scheduler_revision` = NEW.`scheduler_revision`
 			AND p.`scheduler_library_version` = NEW.`scheduler_library_version`
-	) THEN RAISE(ABORT, 'scheduled_completion_stale_boundary') END;
+	) THEN RAISE(ABORT, 'scheduled_completion_stale_boundary') END);
 
-	SELECT CASE WHEN NEW.`queue_class` = 'new' AND (
+	SELECT (CASE WHEN NEW.`queue_class` = 'new' AND (
 		EXISTS (
 			SELECT 1 FROM `learner_case_fsrs` s
 			WHERE s.`user_id` = NEW.`user_id` AND s.`case_id` = NEW.`case_id`
 		)
 		OR NEW.`resulting_state_revision` <> 1
-	) THEN RAISE(ABORT, 'scheduled_completion_stale_case_state') END;
+	) THEN RAISE(ABORT, 'scheduled_completion_stale_case_state') END);
 
-	SELECT CASE WHEN NEW.`queue_class` IN ('due', 'repeat') AND NOT EXISTS (
+	SELECT (CASE WHEN NEW.`queue_class` IN ('due', 'repeat') AND NOT EXISTS (
 		SELECT 1
 		FROM `active_reviews` a
 		INNER JOIN `learner_case_fsrs` s
@@ -86,7 +86,7 @@ BEGIN
 			AND s.`scheduler_revision` = NEW.`scheduler_revision`
 			AND s.`scheduler_library_version` = NEW.`scheduler_library_version`
 			AND NEW.`resulting_state_revision` = s.`state_revision` + 1
-	) THEN RAISE(ABORT, 'scheduled_completion_stale_case_state') END;
+	) THEN RAISE(ABORT, 'scheduled_completion_stale_case_state') END);
 END;
 --> statement-breakpoint
 CREATE TRIGGER `active_reviews_scheduled_completion_expiry_guard`
